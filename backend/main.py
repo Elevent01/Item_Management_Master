ce1dc4aae9581cbc6ccc2b46de010ef9d11440e2
Elevent01 <ritish.modigil@eleventgroup.com>
2026-03-05 16:51:56 +0530

    main.py

--- backend/main.py
+++ backend/main.py
@@ -133,4 +133,8 @@ def health_check():
         }
     }
 
-#   uvicorn main:app --host 0.0.0.0 --port $PORT
+import uvicorn
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
